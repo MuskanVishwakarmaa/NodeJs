106d3281f811a3d183e7a0697f353d74ddf03154
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-18 12:01:16 +0530

    Adding two random dice number

---
+++ addDiceNumber.js
@@ -0,0 +1,6 @@
+let a = Math.floor(Math.random()*6)+1;
+console.log('First dice number: ' + a);
+let b = Math.floor(Math.random()*6)+1;
+console.log('Second dice number: ' + b);
+let c = a+b;
+console.log('sum of two dice number is: ' + c);
